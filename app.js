d4572fd29e8db25946fca69d8bfedcce03a51192
Michelle Bergquist <caker25@gmail.com>
2017-09-18 15:31:17 -0600

    fix path for knexfile

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let router = express.Router()
 // let sqlite3 = require('sqlite3').verbose()
 
 var environment = 'development'
-var config = require('../knexfile')[environment]
+var config = require('./knexfile')[environment]
 let knex = require('knex')(config)
 
 // let routes = require('./routes/theme')
